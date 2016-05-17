cce5b698b6b90c0189148ab12705c59393fe4a2d
YKKKY <1131818177@qq.com>
2016-05-17 18:22:09 +0800

    first submit

---
+++ src/collect_all_even.js
@@ -0,0 +1,24 @@
+function collect_same_elements(collection_a, object_b) {
+    var newArray = [];
+    collection_a.forEach(function (element) {
+        if (isExist(element.key, object_b.value)) {
+
+            newArray.push(element.key);
+        }
+    })
+
+    return newArray;
+}
+
+function isExist(element, collection) {
+
+    for (var i = 0; i < collection.length; i++) {
+        if (element === collection[i]) {
+
+            return true;
+        }
+    }
+
+    return false;
+}
+module.exports = collect_same_elements;
